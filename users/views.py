50aa4db3d16a60b0456df238155952ef6b0a04b3
Elmar <elmar@MacBook-Air-Elmar.local>
2025-08-04 14:05:09 +0500

    Регистрация завершенв

--- users/views.py
+++ users/views.py
@@ -1,16 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form  = UserLoginForm(data=request.POST)
+    
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('shop:index'))
+
+    else:
+        form = UserLoginForm()
+                 
+
     context ={
-        "title":"Home - Авторизация"
+        "title":"Home - Авторизация",
+        'form':form,
     }
     return render(request, "users/login.html" , context)
 
 
 def registration(request):
+    if request.method == 'POST':
+        form  = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('shop:index'))
+
+    else:
+        form = UserRegistrationForm()
+
     context ={
-        "title":"Home - Регистрация"
+        "title":"Home - Регистрация",
+        'form':form,
     }
     return render(request, "users/registration.html" ,context)
 
@@ -23,4 +55,5 @@ def profile(request):
     return render(request, "users/profile.html" ,context)
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('shop:index'))
